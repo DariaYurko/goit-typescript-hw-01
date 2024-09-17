9d9edb3a6b191c6c57dec4a032e000ed07848dd5
Daria <112082575+DariaYurko@users.noreply.github.com>
2024-09-17 10:32:10 +0300

    fix merge

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -3,6 +3,6 @@
  * Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
  */
 
-function merge<T, O>(objA: T, objB: O): T & O {
+function merge<T extends object, O extends object>(objA: T, objB: O) {
   return Object.assign(objA, objB);
 }
